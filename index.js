03041140b2d3fe9955a01efbad5c023400010d0f
inmar salinas <inmarsalinas@yahoo.com>
2023-04-14 18:18:38 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers = ['Antonia', 'Nuru'];
+}
+returnFirstTwoDrivers();
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers = ['Amari', 'Mo'];
+}
+returnLastTwoDrivers();
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier() {
+    return function returnFirstTwoDrivers(){
+        return (5*5);
+    }
+}
+function fareDoubler(fareMultiplier) {
+    return fareMultiplier * 2;
+}
+
+function fareTripler(fareMultiplier) {
+    return fareMultiplier * 3;
+}
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers){
+    return returnFirstTwoDrivers (drivers);
+}
+selectDifferentDrivers();
